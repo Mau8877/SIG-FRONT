e168df79c68ea3671cde9d16c58ecb1a28d12833
Mau8877 <maurogupri@gmail.com>
2026-07-05 16:14:32 -0400

    feat: integrate Redux for state management and add base API setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { Provider } from "react-redux"
 import {
   createRouter,
   RouterProvider,
@@ -7,6 +8,7 @@ import {
 
 import "./index.css"
 import { routeTree } from "./routeTree.gen"
+import { store } from "./store/store"
 
 const router = createRouter({
   routeTree,
@@ -26,6 +28,8 @@ if (!rootElement) {
 
 createRoot(rootElement).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
 )
